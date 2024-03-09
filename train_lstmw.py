7d4fbaf5ec5e8d010a1da5b223d686ed9f237e1d
Greyden <vadercat1@icloud.com>
2024-03-09 15:07:55 -0500

    accounted for precip, hightemp, and lowtemp. also predicted 7 dwarfs

---
+++ train_lstmw.py
@@ -0,0 +1,65 @@
+import tensorflow as tf
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import wait_times
+
+seq_length = 5  # Number of previous time steps to consider
+
+def train_model(ride):
+	data = wait_times.fit_transform_wait_times(ride)
+
+	# Combine dates and times into one input array
+	# these are the columns we'll use as input when we use the model to make a prediction after it's trained
+	X = np.column_stack((data["dates"], data["daysofweek"], data["precip"], data["hightemps"], data["lowtemps"], data["times"]))
+
+	# Create sequences for LSTM
+	X_seq, y_seq = [], []
+	for i in range(len(data["dates"]) - seq_length):
+			X_seq.append(X[i:i + seq_length])
+			y_seq.append(data["waits"][i + seq_length])
+
+	X_seq = np.array(X_seq)
+	y_seq = np.array(y_seq)
+
+	# Split data into training and testing sets
+	X_train, X_test, y_train, y_test = train_test_split(
+			X_seq, # these are the parameters we'll use as input when we want to make a prediction after the model is trained
+			y_seq, # these are actual result values we'll use to train the model on. they are equivalent to the value we want to predict
+			test_size=0.2,
+			random_state=42
+	)
+
+	model = tf.keras.Sequential([
+			tf.keras.layers.Input(shape=(seq_length,6)),
+			tf.keras.layers.LSTM(50, activation='relu'),
+			tf.keras.layers.Dense(1, activation='linear')
+	])
+
+	# Compile the model
+	model.compile(optimizer='adam', loss='mean_squared_error')
+
+	# Train the model
+	model.fit(X_train, y_train, epochs=10, batch_size=16, verbose=1)
+
+	model.summary()
+	model.save(f"models/{ride}_lstmw50.keras")
+
+	# Evaluate the model on the test set
+	loss = model.evaluate(X_test, y_test)
+	print(f'Mean Squared Error on Test Set: {loss}')
+
+if __name__ == "__main__":
+	train_model("7_dwarfs_train")
+	# train_model("alien_saucers")
+	# train_model("dinosaur")
+	# train_model("expedition_everest")
+	# train_model("flight_of_passage")
+	# train_model("kilimanjaro_safaris")
+	# train_model("navi_river")
+	# train_model("pirates_of_caribbean")
+	# train_model("rock_n_rollercoaster")
+	# train_model("slinky_dog")
+	# train_model("soarin")
+	# train_model("spaceship_earth")
+	# train_model("toy_story_mania")
--- wait_times.py
+++ wait_times.py
@@ -2,6 +2,8 @@ import os
 import csv
 import math
 from datetime import datetime, timedelta
+import numpy as np
+from sklearn.preprocessing import StandardScaler
 
 def date_to_int(datestr):
 	date_parts = datestr.split("/")
@@ -19,10 +21,57 @@ def int_to_time(time_int):
 	new_time_object = datetime(1971, 10, 1) + timedelta(seconds=time_int)
 	return new_time_object.strftime('%H:%M:%S')
 
+def prep_wait_times(ride_name, method_name):
+	data_file = f"data/{ride_name}.csv"
+	data = parse_wait_times(data_file)
+	ride_dates = np.array(data["dates"])
+	ride_daysofweek = np.array(data["daysofweek"])
+	ride_precip = np.array(data["precip"])
+	ride_hightemp = np.array(data["hightemps"])
+	ride_lowtemp = np.array(data["lowtemps"])
+	ride_times = np.array(data["times"])
+	ride_durations = np.array(data["waits"])
+
+	# Normalize data
+	#scaler_names = StandardScaler()
+	scaler_dates = StandardScaler()
+	scaler_daysofweek = StandardScaler()
+	scaler_precip = StandardScaler()
+	scaler_hightemp = StandardScaler()
+	scaler_lowtemp = StandardScaler()
+	scaler_times = StandardScaler()
+	scaler_durations = StandardScaler()
+
+	#ride_names_scaled = scaler_names.fit_transform(np.array(rides.values()).reshape(-1, 1))
+	ride_dates_scaled = getattr(scaler_dates, method_name)(ride_dates.reshape(-1, 1))
+	ride_daysofweek_scaled = getattr(scaler_daysofweek, method_name)(ride_daysofweek.reshape(-1, 1))
+	ride_precip_scaled = getattr(scaler_precip, method_name)(ride_precip.reshape(-1, 1))
+	ride_hightemp_scaled = getattr(scaler_hightemp, method_name)(ride_hightemp.reshape(-1, 1))
+	ride_lowtemp_scaled = getattr(scaler_lowtemp, method_name)(ride_lowtemp.reshape(-1, 1))
+	ride_times_scaled = getattr(scaler_times, method_name)(ride_times.reshape(-1, 1))
+	ride_durations_scaled = getattr(scaler_durations, method_name)(ride_durations.reshape(-1, 1))
+	return {
+		"dates": ride_dates_scaled,
+		"daysofweek": ride_daysofweek_scaled,
+		"precip": ride_precip_scaled,
+		"hightemps": ride_hightemp_scaled,
+		"lowtemps": ride_lowtemp_scaled,
+		"times": ride_times_scaled,
+		"waits": ride_durations_scaled
+	}
+
+def transform_wait_times(ride_name):
+	return prep_wait_times(ride_name, "transform")
+
+def fit_transform_wait_times(ride_name):
+	return prep_wait_times(ride_name, "fit_transform")
 
 def parse_wait_times(filename):
 	dates = []
 	daysofweek = []
+	hightemps = []
+	lowtemps = []
+	precip = []
 	times = []
 	waits = []
 	metadata = {}
@@ -30,7 +79,10 @@ def parse_wait_times(filename):
 		csv_reader = csv.DictReader(csvfile, delimiter=",")
 		for row in csv_reader:
 			metadata[row['DATE']] = {
-				'dayofweek': 0,
+				'dayofweek': row["DAYOFWEEK"],
+				'precip': row["WEATHER_WDWPRECIP"],
+				'high_temp': row["WEATHER_WDWHIGH"],
+				'low_temp': row["WEATHER_WDWLOW"]
 			}
 	with open(filename, "r") as csvfile:
 		csv_reader = csv.DictReader(csvfile, delimiter=",")
@@ -38,9 +90,20 @@ def parse_wait_times(filename):
 			if row["SPOSTMIN"] and int(row["SPOSTMIN"]) >= 0 and row["date"] in metadata:
 				dates.append(date_to_int(row["date"]))
 				daysofweek.append(int(metadata[row["date"]]['dayofweek']))
+				precip.append(float(metadata[row["date"]]["precip"]))
+				hightemps.append(float(metadata[row["date"]]["high_temp"]))
+				lowtemps.append(float(metadata[row["date"]]["low_temp"]))
 				times.append(time_to_int(row["datetime"].split()[1]))
 				waits.append(int(row["SPOSTMIN"]))
-	return dates, daysofweek, times, waits
+	return {
+		"dates": dates,
+		"daysofweek": daysofweek,
+		"precip": precip,
+		"hightemps": hightemps,
+		"lowtemps": lowtemps,
+		"times": times,
+		"waits": waits
+	}
 
 
 
